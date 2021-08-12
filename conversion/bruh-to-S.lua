c566eede8269683734100496235b3fbe75fca9cc
Saiapatsu <26184960+Saiapatsu@users.noreply.github.com>
2021-08-12 21:59:57 +0300

    Move sheet layouts to common

--- conversion/bruh-to-S.lua
+++ conversion/bruh-to-S.lua
@@ -2,26 +2,13 @@ local script, from, to = table.unpack(args)
 local path = require("path")
 local common = require(path.resolve(path.dirname(table.remove(args, 1)), "..", "common.lua"))
 
-local stride = 6
-local src = {
-	170, 138, 69,   8, 21, 168,
-	 42,  10, 17,  80, 68,  40,
-	 85,  65,  4,   5,  1,  20,
-	 34,   2, 16, 128, 64,  32,
-	162, 130, 84, 136, 81, 160,
-}
-
-local w = 16
-local h = 2
-local dst = {
-	0, 2, 8, 10, 32, 34, 40, 42, 128, 130, 136, 138, 160, 162, 168, 170,
-	0, 1, 4,  5, 16, 17, 20, 21,  64,  65,  68,  69,  80,  81,  84,  85,
-}
+local src = common.layouts.bruh
+local dst = common.layouts.S
 
 local src2 = {}
 for i,v in ipairs(src) do
-	local x = (i  -1)%stride
-	local y = (i-x-1)/stride
+	local x = (i  -1)%src.w
+	local y = (i-x-1)/src.w
 	src2[v] = common.unparse(from) .. "[32x32+" .. x*32 .. "+" .. y*32 .. "]"
 end
 src2[0] = "-size 32x32 xc:#00000000"
@@ -33,7 +20,7 @@ end
 os.execute(table.concat({
 	"magick montage",
 	"-background none",
-	"-tile " .. w .. "x" .. h,
+	"-tile " .. dst.w .. "x" .. dst.h,
 	"-geometry +0+0",
 	table.concat(dst, " "),
 	common.unparse(to),
