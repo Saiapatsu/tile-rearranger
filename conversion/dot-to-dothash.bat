3049a4884f9f08955da4790774c3df1d4fa3a440
Saiapatsu <26184960+Saiapatsu@users.noreply.github.com>
2021-08-26 04:32:52 +0300

    Remove an obsolete batch converter

--- conversion/dot-to-dothash.bat
+++
@@ -1,9 +0,0 @@
-magick ^
-	( %1[32x32+32+64] ( +clone +clone ) +append ( +clone ) -append -repage +00+32 ) ^
-	( %1[32x32+00+32] ( +clone +clone ) -append ( +clone ) +append -repage +00+00 ) ^
-	( %1[32x32+32+00] ( +clone +clone ) +append ( +clone ) -append -repage +00+00 ) ^
-	( %1[32x32+64+32] ( +clone +clone ) -append ( +clone ) +append -repage +32+00 ) ^
-	-background #00000000 -flatten ^
-	%1 +swap +append ^
-	%~2
-if NOT "%errorlevel%"=="0" pause
